5691e827992716bb64d0ba9c3b259627a70f08bc
Andresvilla27 <motorolasmartflip@gmail.com>
2022-09-02 10:50:17 -0300

    creacion de componentes

--- proyecto1/src/app/app.module.ts
+++ proyecto1/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    HabilidadesComponent,
+    ExperienciaComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
