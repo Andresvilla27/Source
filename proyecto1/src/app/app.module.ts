78f14d1525a41beaf120b80ecd68976ed7ef819b
Andresvilla27 <motorolasmartflip@gmail.com>
2022-09-19 00:04:22 -0300

    creacion de Login - servicio auth- y componente login

--- proyecto1/src/app/app.module.ts
+++ proyecto1/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
 import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ContactoComponent } from './componentes/contacto/contacto.component';
     AcercaDeComponent,
     HabilidadesComponent,
     ExperienciaComponent,
-    ContactoComponent
+    ContactoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
